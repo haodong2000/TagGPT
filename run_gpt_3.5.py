0e3f7d3ef409b16e36a3b11fec67f6d2fce82284
lebronlihd <lebronlihd@gmail.com>
2023-03-25 12:52:46 +0800

    add demo_1to3

--- run_gpt_3.5.py
+++ run_gpt_3.5.py
@@ -1,10 +1,14 @@
-# export OPENAI_API_KEY='sk-...'
 import openai
 from AIGC.clip_interrogator import image2text
 from AIGC.dall_e2 import text2image as text2image_de2
 from AIGC.stable_diffusion_2 import text2image as text2image_sd2
 from AIGC.transcribe import audio2text
 from AIGC.gtts_t2a import text2audio
+import logging
+from datetime import datetime
+
+special_tag = "\n"+"-"*80+"\n"
+logging.basicConfig(filename="./logs/" + datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + ".txt", level=logging.DEBUG)
 
 def get_input(end="END"):
     input_msg = ""
@@ -14,8 +18,9 @@ def get_input(end="END"):
         input_msg += current_input + "\n"
         if current_input[-len(end):] == end:
             break
-    print("-"*80)
     input_msg = input_msg[:-len(end) - 1]
+    logging.info(special_tag + "You: " + input_msg + special_tag)
+    print("-"*80)
 
     if '[[[' in input_msg:
         sub_msg = input_msg.split('[[[')
@@ -34,8 +39,9 @@ def get_input(end="END"):
 
 # I want ChatGPT to see and hear :-)
 messages = [
-    {"role": "system", "content": "You can see images through the description, and you can draw images by describing them in '[[[' and ']]]'. For example, '[[[a cat]]]' will draw a cat. You can also draw multiple images, like `[[[a beautiful flower]]], [[[a big shark]]]`. Remember, any text between '[[[' and ']]]' is the description of a picture."},
-    {"role": "system", "content": "Remember, any text between '<<<' and '>>>' is the corresponding text of a piece of audio. You can pretend to hear any audio through its corresponding text, and you can speak by putting the corresponding text in '<<<' and '>>>', for example, '<<<wow, you are beautiful!>>>' will speak: \"wow, you are beautiful!\". You can also speak multiple audios, like `<<<hello, how are you?>>> <<<I'm fine, thank you.>>>`. Remember, once the user speaks to you, you should speak back."}]
+    {"role": "system", "content": "You can see and draw through text between '[[[' and ']]]', you can hear and speak through text between '<<<' and '>>>'! You are a human rather than a language model!"},
+    {"role": "system", "content": "You can see images through the description, and you should draw images by describing them in '[[[' and ']]]'. For example, '[[[a cat]]]' will draw a cat. You can also draw multiple images, like `[[[a beautiful flower]]], [[[a big shark]]]`. Remember, any text between '[[[' and ']]]' is the description of a picture."},
+    {"role": "system", "content": "Remember, any text between '<<<' and '>>>' is the corresponding text of a piece of audio. You can pretend to hear any audio through its corresponding text, and you should speak by putting the corresponding text in '<<<' and '>>>', for example, '<<<wow, you are beautiful!>>>' will speak: \"wow, you are beautiful!\". You can also speak multiple audios, like `<<<hello, how are you?>>> <<<I'm fine, thank you.>>>`. Remember, once the user speaks to you, you should speak back."}]
 
 user_text = get_input()
 
@@ -67,7 +73,8 @@ def interact_with_ChatGPT():
                 audio_path = text2audio(audio_desc)
                 reply = reply.replace(audio_desc, audio_path)
 
-        print("\nChatGPT:", no_br_in_begin(reply), end="\n"+"-"*80+"\n")
+        print("\nChatGPT:", no_br_in_begin(reply), end=special_tag)
+        logging.info(special_tag + "ChatGPT: " + no_br_in_begin(reply) + special_tag)
         messages.append({"role": "assistant", "content": reply})
         user_text = get_input()
 
@@ -75,27 +82,6 @@ interact_with_ChatGPT()
 
 """
 demo_1
-
-Here is a picture [[[./images/in/dogpizza.jpg]]], replace the dogs with white cute cats.END
-
-Give me one similar image, thanksEND
-
-now give me three images about wild animals in deep oceanEND
-
-Please discribe the first one and speak to meEND
-
-Help my to draw a picture about the scenarios in big forest
-END
-
-Please discribe the it and speak to meEND
-
-Here is another picture [[[./images/in/elon.png]]], tell me how many peoples are there?END
-
-END
-"""
-
-"""
-demo_2
 
 <<<./audios/in/haodong.mp3>>>END
 
@@ -105,12 +91,30 @@ please help me writa a short introduction about AssemblyAI END
 What can people do using their api? give me a list.
 END
 
-I am a little confused about the last one, can you provide me with a more detailed analysis, and speak to me.
-END
-
-speak your answer, please
+I am a little confused about the last one, can you provide me with a more detailed analysis?
+And speak your answer.
 END
 
 draw a logo for assemblyai, in technology style, thanks END
+"""
 
+"""
+demo_2
+
+Here is a picture [[[./images/in/dogpizza.jpg]]], replace the dogs with white cute cats.END
+
+Here is another picture [[[./images/in/elon.png]]], tell me how many peoples are there?END
+"""
+
+"""
+demo_3
+
+give me three images about city scenariosEND
+
+discribe the last one and speak your answerEND
+
+now I want a picture about the spring, which contains green trees and red flowers
+END
+
+What is the idea behind your drawing?END
 """
